64f69e8b0220984c585b3597af000cbc21bde79c
Hunter Czarapata <czarapata.hunter@gmail.com>
2022-09-09 16:27:35 -0700

    Added ask.Button Event Listener and console.log(fortune);

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inputsContainer = document.getElementById('inputs-container');
 const fortuneContainer = document.getElementById('fortune-container');
-
+console.log(fortune);
 const askButton = document.getElementById('ask');
 const restartButton = document.getElementById('restart');
 
@@ -27,3 +27,11 @@ const answers = [
     'Outlook not so good',
     'Very doubtful',
 ];
+
+askButton.addEventListener('click', () => {
+    const randNum = Math.floor(Math.random() * answers.length);
+    fortune.textContent = answers[randNum];
+
+    inputsContainer.classList.toggle('hide');
+    fortuneContainer.classList.toggle('hide');
+});
