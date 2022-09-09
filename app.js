71616d009ec745ee8c79a0c1ade2bbc56c0014a9
Hunter Czarapata <czarapata.hunter@gmail.com>
2022-09-09 15:05:57 -0700

    Changes to HTML and starting of Java const

---
+++ app.js
@@ -0,0 +1,7 @@
+const inputs = document.getElementById('inputs');
+const fortuneContainer = document.getElementById('fortune-container');
+
+const askButton = document.getElementById('ask-fortune');
+const restartButton = document.getElementById('restart');
+
+const fortune = document.getElementById('fortune');
